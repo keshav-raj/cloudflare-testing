a2501a5ecc0152ae0c75f9778a900a0189f140cc
Keshav Raj <keshavkuamrk7@gmail.com>
2022-03-23 13:11:05 +0530

    Cloudflare

---
+++ static/locales/en/index.ts
@@ -0,0 +1,18 @@
+import enCommon from './common.json';
+import enProduct from './product.json';
+import enPayment from './payment.json';
+import enDiscussion from './discussion.json';
+import enTestPlayer from './testPlayer.json';
+import enNewsfeed from './newsfeed.json';
+import enPlayer from './player.json';
+import enResult from './result.json';
+import enAccount from './account.json';
+import enSupport from './support.json';
+import enTestSeriesAnalytics from './testSeriesAnalytics.json'
+import enError from './error.json'
+import enInvite from './invite.json'
+import enProfileFields from './profileFields.json'
+
+const enResources = { enCommon, enProduct, enPayment, enTestPlayer, enNewsfeed, enPlayer, enResult, enDiscussion, enAccount, enSupport, enTestSeriesAnalytics, enError, enInvite, enProfileFields }
+
+export default enResources;
---
+++ static/locales/hindi/index.ts
@@ -0,0 +1,12 @@
+import hindiCommon from './common.json';
+import hindiProduct from './product.json';
+import hindiTestPlayer from './testPlayer.json';
+import hindiNewsfeed from './newsfeed.json';
+import hindiResult from './result.json';
+import hindiAccount from './account.json';
+import hindiSupport from './support.json';
+import hindiTestSeriesAnalytics from './testSeriesAnalytics.json'
+
+const hindiResources = { hindiCommon, hindiProduct, hindiTestPlayer, hindiNewsfeed, hindiResult, hindiAccount, hindiSupport,hindiTestSeriesAnalytics}
+
+export default hindiResources;
